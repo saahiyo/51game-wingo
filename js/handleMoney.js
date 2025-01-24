166214f7c6d9584d12cb2bd26c41a0ab2881f684
saahiyo <linkg518@gmail.com>
2025-01-24 23:39:55 +0530

    Refactor money handling logic to calculate winnings and update balance display

--- js/handleMoney.js
+++ js/handleMoney.js
@@ -1,11 +1,20 @@
-import { money } from "./elements.js";
-import { newBalance } from "./events.js";
+import { money , winBonus} from "./elements.js";
+import { newBalance, isBetted } from "./events.js";
 import { gameData } from "./gameConfig.js";
 import { period_number } from "./elements.js";
 import { totalBetAmount } from "./events.js";
 
 export function handleMoneys(){
-    let myMoney = newBalance
-    console.log(myMoney += 1);
-    console.log("bet amount is: "+ totalBetAmount);
+    // let myMoney = newBalance
+    let bettedMoney = totalBetAmount
+    let deduction = (bettedMoney / 100) * 2;
+    let remainingAmount = bettedMoney - deduction;
+    let multipliedMoney = remainingAmount * 2
+    // console.log("multiplied money: ",multipliedMoney);    
+    let currentBalance = parseFloat(money.textContent.replace('₹', '').replace(/,/g, ''));
+    currentBalance += multipliedMoney;
+    if(isBetted){
+        winBonus.textContent = `₹${multipliedMoney.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+        money.textContent = `₹${currentBalance.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+    }
 }
